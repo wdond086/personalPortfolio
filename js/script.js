e7969538e4deda84156040532ff21b87f6f39251
wdond086 <tdondjiwilfried@hotmail.com>
2018-11-08 22:11:35 -0500

    final mobile version finished

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 $('.menu-toggle').click(function() {
-
     $('.site-nav').toggleClass('site-nav-open', 500);
     $(this).toggleClass('open');
+})
 
+$('body').click(function() {
+    if ($('#navbar').hasClass('site-nav-open')) {
+        $('.site-nav').toggleClass('site-nav-open', 500);
+        $('.menu-toggle').toggleClass('open');
+    }
 })
